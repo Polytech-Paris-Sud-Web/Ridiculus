594c7397d2dcb43adc636657b2227b5cbb01919a
cregis <regis.cassier@u-psud.fr>
2020-05-04 19:15:49 +0200

    add interaction (not linked with service)

--- src/app/poste/post.class.ts
+++ src/app/poste/post.class.ts
@@ -8,10 +8,10 @@ export interface Poste {
     modificator : string,
     dateModificator : Date,
     vote : number
-};
+}
 
 export interface CreatePoste {
     title : string,
     content : string,
     author : string
-};
+}
--- src/app/poste/poste.component.ts
+++ src/app/poste/poste.component.ts
@@ -11,9 +11,42 @@ export class PosteComponent implements OnInit {
   @Input('poste')
   poste: Poste;
 
+  userOfflineAccess: boolean;
+  userVote: number;
+
   constructor() { }
 
   ngOnInit(): void {
+    // TODO update with service;
+    this.userOfflineAccess = true;
+    this.userVote = 0;
+  }
+
+  changeVote(vote: number): void{
+    // TODO use service
+    this.poste.vote += vote - this.userVote;
+    this.userVote = vote;
+  }
+
+  toggleOfflinePost(): void {
+    
+    this.userOfflineAccess = !this.userOfflineAccess;
+
+    if(this.userOfflineAccess) {
+      this.addOfflinePost();
+    } else {
+      this.removeOfflinePost();
+    }
+  }
+
+  addOfflinePost(): void {
+    // TODO
+    throw new Error("Method not implemented.");
+  }
+
+  removeOfflinePost(): void {
+    // TODO
+    throw new Error("Method not implemented.");
   }
 
 }
