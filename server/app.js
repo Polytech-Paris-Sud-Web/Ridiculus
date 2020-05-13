8e7ac1ca19dfeafd282d46611f5433ced9f6916c
SireConflexe <valentin.dty@gmail.com>
2020-05-13 23:47:42 +0200

    update server with https

--- server/app.js
+++ server/app.js
@@ -3,6 +3,8 @@ const app = express()
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const cors = require('cors');
+const fs = require('fs')
+const https = require('https')
 
 app.use(bodyParser.json())
 app.use(cors());
@@ -23,5 +25,16 @@ app.get('/', (req, res) => {
 //Connect to mongodb
 mongoose.connect('mongodb://localhost:27017/myapp', {useNewUrlParser: true}, () => console.log('Connected to DB'))
 
-//start listening the server
-app.listen(3000)
+//start listening the server https
+https.createServer({
+    key: fs.readFileSync('server.key', 'utf8'),
+    cert: fs.readFileSync('server.cert', 'utf8')
+  }, app)
+  .listen(3000, function () {
+    console.log('Ca tourne sur le port 3000!')
+})
+
+// running http server
+const http = require('http')
+const httpServer = http.createServer(app)
+httpServer.listen(3001)
