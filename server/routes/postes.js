1e8fa1e082c94eeaffb41de293d1106dad8b727a
SireConflexe <valentin.dty@gmail.com>
2020-05-13 18:52:09 +0200

    fixe parsing of date

--- server/routes/postes.js
+++ server/routes/postes.js
@@ -33,7 +33,7 @@ router.post('/', async (req,res) => {
         content: req.body.content,
         author: req.body.author,
         dateCreated: req.body.dateCreated,
-        dateUpdated: JSON.stringify(new Date()),
+        dateUpdated: JSON.parse(JSON.stringify(new Date())),
         vote: 0
     })
     
